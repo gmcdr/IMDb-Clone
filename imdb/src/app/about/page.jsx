d3cc469cff7267725dfe72af0e739615ed81ed1b
Gabriel Reis <gabriel.mcd.reis@gmail.com>
2023-09-12 09:45:27 -0300

    Criação da about section

---
+++ imdb/src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-500' >About</h1>
+      <p>Welcome to IMDb, your ultimate destination for all things cinema and television! We are passionate about movies, TV shows, and the talented individuals who bring them to life, and we've created this platform to celebrate and explore the world of entertainment with you.
+      </p>
+      <p>Our mission is to provide a comprehensive and user-friendly database of movies and TV series, complete with detailed information on cast, crew, plot summaries, and reviews. Whether you're a seasoned film buff or a casual viewer, you'll find everything you need to enhance your viewing experience right here.</p>
+      <p>Discover the world of cinema and television like never before with IMDb. Our platform is your ultimate destination for comprehensive movie and TV show information, in-depth cast and crew details, user reviews, personalized recommendations, and the latest entertainment news. Join a passionate community of film and TV enthusiasts and elevate your entertainment experience. Explore, engage, and connect with IMDb today.</p>
+    </div>
+  )
+}
+
+export default About
