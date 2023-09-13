dbde951711642b4da3c9810ed9b3d8c07c415824
Gabriel Reis <gabriel.mcd.reis@gmail.com>
2023-09-13 14:08:20 -0300

    add: Movie page

--- imdb/next.config.js
+++ imdb/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+  images:{
+    domains:[
+      "image.tmdb.org"
+    ]
+  }
+
+}
